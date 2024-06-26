f5bb29946f2e442e07ef6f56c7d29699e0596586
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-06-26 21:24:46 +0200

    development plan

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import pygame
+
+#basic pygame scriipt
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("First Game")
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+    clock.tick(60)
