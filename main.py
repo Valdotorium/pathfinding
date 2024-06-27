553058c2c1f261269f7db12de5490aeee8a94cb2
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-06-27 09:06:25 +0200

    pygbag ready

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import pygame
+import asyncio
+async def main():
+    #basic pygame scriipt
 
-#basic pygame scriipt
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("project phoenix")
+    clock = pygame.time.Clock()
+    running = True
 
-pygame.init()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-screen = pygame.display.set_mode((800,600))
+        pygame.display.update()
+        screen.fill((0,0,0))
+        clock.tick(60)
 
-pygame.display.set_caption("First Game")
-
-clock = pygame.time.Clock()
-
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    pygame.display.update()
-    clock.tick(60)
+if __name__ == "__main__":
+    asyncio.run(main())
