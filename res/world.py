a91142a7483e0ff8e3b4a79cb0dbe8cb80af4693
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-06-29 15:30:31 +0200

    getting hovered tiles

--- res/world.py
+++ res/world.py
@@ -10,7 +10,7 @@ def createWorld(worldsize):
     return world
 def draw(screen, world, zoom, cameraPos, tileSize, screenSize):
     #draw the matrix of tiles on the screen
-
+    zoomedTileSize = tileSize / zoom 
     for x in range(world["size"]):
         for y in range(world["size"]):
             tileX = (x - cameraPos[0]) / zoom
@@ -18,6 +18,14 @@ def draw(screen, world, zoom, cameraPos, tileSize, screenSize):
 
             if tileX >= 0 and tileX < screenSize[0] and tileY >= 0 and tileY < screenSize[1]:
                 tileColor = (255, 255, 255) if world["tiles"][x][y] == 0 else (0, 0, 0)
-                pygame.draw.rect(screen, tileColor, (tileX * tileSize, tileY * tileSize, tileSize / zoom, tileSize / zoom), 3)
+                pygame.draw.rect(screen, tileColor, (tileX * tileSize, tileY * tileSize, zoomedTileSize, zoomedTileSize), 3)
+    #based on x and y position of the mouse and the camera, get the coordinates of the tiles the mouse is hovering above
+    mouseX, mouseY = pygame.mouse.get_pos()
+    tileX, tileY = (mouseX / zoomedTileSize) + cameraPos[0], (mouseY / zoomedTileSize) + cameraPos[1]
+    print(tileX, tileY)
+    if tileX >= 0 and tileX < world["size"] and tileY >= 0 and tileY < world["size"]:
+        tileX -= cameraPos[0]
+        tileY -= cameraPos[1]
+        pygame.draw.rect(screen, (255, 0, 0), (int(tileX) * zoomedTileSize, int(tileY) * zoomedTileSize, zoomedTileSize, zoomedTileSize))
 
 
